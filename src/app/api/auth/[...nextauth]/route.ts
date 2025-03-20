4a447676f8f0f494dba9e43fdbbaf3f1d7c86d8f
Zeereoreo <wltjrcjf12@naver.com>
2025-03-21 00:14:03 +0900

    fix : authOptions문제에 따른 로그인 실패 오류해결

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,33 +2,7 @@ import NextAuth from "next-auth";
 import NaverProvider from "next-auth/providers/naver";
 import KakaoProvider from "next-auth/providers/kakao";
 import GoogleProvider from "next-auth/providers/google";
-
-const authOptions = {
-  providers: [
-    NaverProvider({
-      clientId: process.env.NAVER_ID as string,
-      clientSecret: process.env.NAVER_SECRET as string,
-    }),
-    KakaoProvider({
-      clientId: process.env.KAKAO_ID as string,
-      clientSecret: process.env.KAKAO_SECRET as string,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
-    }),
-  ],
-  pages: {
-    signIn: '/login',
-  },
-};
+import { authOptions } from "@/api/utils/authOptions";
 
 const handler = NextAuth(authOptions);
-
-export async function GET(request: Request) {
-  return handler(request);
-}
-
-export async function POST(request: Request) {
-  return handler(request);
-}
+export { handler as GET, handler as POST }; 
